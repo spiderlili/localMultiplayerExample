af0fcb05ecad9cb4f862c33282e6a452c95cc98d
Jing Chun Tan <jtan1993@gmail.com>
2018-04-06 16:32:24 +0100

    Add files via upload

---
+++ NetworkLobbyHook.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+
+public class NetworkLobbyHook : LobbyHook 
+{
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer) 
+    { 
+        LobbyPlayer lPlayer = lobbyPlayer.GetComponent<LobbyPlayer>();
+
+        PlayerSetup pSetup = gamePlayer.GetComponent<PlayerSetup>();
+
+        pSetup.m_name = lPlayer.playerName;
+        pSetup.m_playerColor = lPlayer.playerColor;
+    
+    
+        PlayerManager pManager = gamePlayer.GetComponent<PlayerManager>();
+        if (pManager != null)
+        {
+            GameManager.m_allPlayers.Add(pManager);
+        }
+    
+    }
+
+
+}
---
+++ SpawnPoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class SpawnPoint : NetworkBehaviour 
+{
+
+
+	public bool m_isOccupied;
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if ( other.gameObject.tag == "Player")
+		{
+			m_isOccupied = true;
+		}
+	}
+
+
+	void OnTriggerStay (Collider other)
+	{
+		if ( other.gameObject.tag == "Player")
+		{
+			m_isOccupied = true;
+		}
+	}
+
+   
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			m_isOccupied = false;
+		}
+	}
+
+
+}
